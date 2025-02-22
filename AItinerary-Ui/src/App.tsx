d1f7caf79ade810f8eb2eead50b14b8ef8874adf
lmf6356 <lmf6356@rit.edu>
2025-02-22 14:09:38 -0500

    fixed the fact that we pushed the api key

--- AItinerary-Ui/src/App.tsx
+++ AItinerary-Ui/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { APIProvider, Map } from '@vis.gl/react-google-maps'
 
 function App() {
   const [count, setCount] = useState(0)
